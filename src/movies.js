047263b5fa2162a8045f1fa24810a9e773283595
Nil Valero <nvalero13@gmail.com>
2022-11-03 22:06:25 +0100

    First 3 iterations

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,42 @@
+/*
+  [
+    {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+    },
+  ]
+*/
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    return moviesArray.map((el) => el.director);
+    // BONUS
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  return moviesArray.filter(
+    (el) => el.director === "Steven Spielberg" && el.genre.includes("Drama")
+  ).length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if (moviesArray.length === 0) return 0;
+  else {
+    const scoresSum = moviesArray.reduce((a, b) => {
+      return b.score ? a + b.score : a;
+    }, 0);
+    const scoresAvg = scoresSum / moviesArray.length;
+    return Math.round(scoresAvg * 100) / 100;
+  }
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
